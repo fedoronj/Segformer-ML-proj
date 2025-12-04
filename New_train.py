80388f4e76d5b7de8f04b75ebfe5078f5aa70fda
Joseph Fedoronko <91025754+fedoronj@users.noreply.github.com>
2025-12-03 18:09:02 -0600

    Create New_train.py

---
+++ New_train.py
@@ -0,0 +1,24 @@
+from torch.utils.data import DataLoader
+import torch
+
+dataset = ...                         # your lane dataset
+dataloader = DataLoader(dataset, batch_size=4, shuffle=True)
+
+optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4)
+
+model.train().cuda()
+
+for epoch in range(10):
+    for batch in dataloader:
+        images = batch["pixel_values"].cuda()
+        labels = batch["labels"].cuda()
+
+        logits = model(pixel_values=images)
+
+        loss = torch.nn.functional.cross_entropy(logits, labels)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        print("loss:", loss.item())
